fd6b041d7288273a63ecfcf9de471ddd541b8d2d
Ju Dayoung <ekdud7703@naver.com>
2022-01-29 16:11:27 +0900

    DistanceFarePolicy 수정, DistanceFarePolicyTest 테스트 완료

--- src/main/java/com/judy/price/fare/DistanceFarePolicy.java
+++ src/main/java/com/judy/price/fare/DistanceFarePolicy.java
@@ -7,6 +7,8 @@ import static com.judy.price.member.Grade.*;
 
 public class DistanceFarePolicy implements FarePolicy{
 
+    int basicFare=1250;
+
     @Override
     public int getFare(Member member, int distance) {
         Grade grade = member.getGrade();
@@ -26,17 +28,17 @@ public class DistanceFarePolicy implements FarePolicy{
     }
 
     public int getAdultFare(int distance){
-        int fare=1250;  // 10km 이내
+        int fare=basicFare;  // 10km 이내
         int oneStep=10;
         int twoStep=50;
         int farePerDistance=100;
 
-        if(distance > oneStep && distance < twoStep){
-            int extraFare = (distance-oneStep)/5 * farePerDistance;
+        if(distance > oneStep && distance <= twoStep){
+            int extraFare = (int) Math.ceil( (double) (distance-oneStep) / 5 ) * farePerDistance;
             fare += extraFare;
         }else if(distance > twoStep){
             fare += (twoStep-oneStep)/5 * farePerDistance;
-            int extraFare = (distance-twoStep)/8 * farePerDistance;
+            int extraFare = (int) Math.ceil( (double) (distance-twoStep) / 8 ) * farePerDistance;
             fare += extraFare;
         }
         return fare;
---
+++ src/test/java/com/judy/price/fare/DistanceFarePolicyTest.java
@@ -0,0 +1,38 @@
+package com.judy.price.fare;
+
+import com.judy.price.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DistanceFarePolicyTest {
+
+    DistanceFarePolicy farePolicy=new DistanceFarePolicy();
+
+    @Test
+    @DisplayName("회원은 등급에 따라 요금이 달라져야 한다.")
+    void getFare() {
+        Member etc=new Member(1L, "etc", true, 1980);
+        Member oldman=new Member(2L, "oldman", false, 1930);
+        Member adult=new Member(2L, "adult", false, 1980);
+        Member youth=new Member(2L, "youth", false, 2007);
+        Member child=new Member(2L, "child", false, 2012);
+
+        assertThat(farePolicy.getFare(etc, 58)).isEqualTo(0);
+        assertThat(farePolicy.getFare(oldman, 58)).isEqualTo(0);
+        assertThat(farePolicy.getFare(adult, 58)).isEqualTo(farePolicy.basicFare+900);
+        assertThat(farePolicy.getFare(youth, 58)).isEqualTo((farePolicy.basicFare+900-350)*8/10);
+        assertThat(farePolicy.getFare(child, 58)).isEqualTo((farePolicy.basicFare+900-350)*5/10);
+    }
+
+    @Test
+    @DisplayName("성인 요금은 거리(3구간)에 따라 요금이 달라져야 한다.")
+    void getAdultFare() {
+        assertThat(farePolicy.getAdultFare(8)).isEqualTo(farePolicy.basicFare);
+        assertThat(farePolicy.getAdultFare(14)).isEqualTo(farePolicy.basicFare + 100);
+        assertThat(farePolicy.getAdultFare(16)).isEqualTo(farePolicy.basicFare + 200);
+        assertThat(farePolicy.getAdultFare(50)).isEqualTo(farePolicy.basicFare + 800);
+        assertThat(farePolicy.getAdultFare(58)).isEqualTo(farePolicy.basicFare + 900);
+    }
+}
